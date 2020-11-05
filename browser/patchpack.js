42db5c69a46e6093f6ca02996f9781df0fd7fbc2
Damir <damir@yusipov.ru>
2020-11-06 00:45:42 +0300

    version fix

--- browser/patchpack.js
+++ browser/patchpack.js
@@ -1,4 +1,4 @@
-/*! patchpack@0.2.2 */
+/*! patchpack@0.2.3 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
